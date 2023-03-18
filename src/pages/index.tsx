d5aace6a8a7bb245fe35836789f68e2b179e64f9
Rafael Gómez <satrgomez7@gmail.com>
2023-03-18 19:20:33 -0400

    fix: remove unnecessary files/ format code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { Octokit } from 'octokit'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import parseData from '@/utils/parseData'
 import { CommitInfo } from '@/utils/interfaces'
 
