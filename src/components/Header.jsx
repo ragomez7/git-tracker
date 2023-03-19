dfb3981db7c98a3a1a9f7b57e213db2ed96e9451
Rafael Gómez <satrgomez7@gmail.com>
2023-03-18 20:44:06 -0400

    build: clean a bit and style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className="flex justify-center">Git Tracker</header>
+        <header className="flex justify-center text-xl">Git Tracker</header>
     )
 }
