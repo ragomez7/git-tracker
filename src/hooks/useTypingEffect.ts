9bb7499e52b463ba844b6e2fedcaec105da91241
Rafael Gómez <satrgomez7@gmail.com>
2023-03-20 09:13:13 -0400

    refactor: modified text-slicing

--- src/hooks/useTypingEffect.ts
+++ src/hooks/useTypingEffect.ts
@@ -7,7 +7,7 @@ export default function useTypingEffect(text: string) {
     if (count <= text.length) {
       setTimeout(() => {
         setCount(count + 1)
-        setRenderedText(text.split('').splice(0, count).join(''))
+        setRenderedText(text.slice(0, count))
       }, 20)
     }
   }, [count, text])
