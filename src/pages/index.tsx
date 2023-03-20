5cf6cfe79aee82cceb37ec086cc0cb1ec9f79378
Rafael Gómez <satrgomez7@gmail.com>
2023-03-19 20:56:03 -0400

    fix: await getCommits()

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ interface HomeProps {
 }
 
 export async function getServerSideProps() {
-  const parsedCommits = getCommits();
+  const parsedCommits = await getCommits();
   return { props: { parsedCommits } }
 }
 
